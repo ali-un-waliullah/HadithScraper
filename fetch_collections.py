8527855ad4d09e416ebc9467e45cf33d12f37ea3
Sayyed-Salman <sayyedsalman2000@gmail.com>
2023-05-23 04:43:01 +0530

    Add pagination downloads

--- fetch_collections.py
+++ fetch_collections.py
@@ -1,15 +1,35 @@
 import requests
 
 
-def get_collections(parms: dict) -> bool:
+def get_collections(search_query: str) -> bool:
     """
-    Fetches the collections from sunnah.com
+    Fetches the collections from sunnah.com based on a search query
     """
+    query = {
+        "q": f"{search_query}"
+    }
     url = "https://sunnah.com"
-    response = requests.get(url + "/search?", params=parms)
+    response = requests.get(url + "/search?", params=query)
     if response.status_code == 200:
-        with open('search.html', 'w') as f:
+        with open('./pages/search.html', 'w', encoding='utf-8') as f:
             f.write(response.content.decode('utf-8'))
         return True
     print("Error: ", response.status_code)
     return False
+
+
+def fetch_page(url: str, filename: str) -> bool:
+    """
+    Fetches the pages from sunnah.com based on a url
+    """
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(f"./pages/{filename}", 'w', encoding='utf-8') as f:
+            f.write(response.content.decode('utf-8'))
+        return True
+    print("Error: ", response.status_code)
+    return False
+
+
+if __name__ == "__main__":
+    get_collections("abu")
--- process_collections.py
+++ process_collections.py
@@ -1,22 +1,7 @@
 from bs4 import BeautifulSoup
 import json
 import re
-
-
-with open('search.html', 'r', encoding='utf-8') as f:
-    soup = BeautifulSoup(f.read(), 'html.parser')
-
-# Open file for reading
-# Read and parse the file
-# Create json objects in /collections with the following format:
-# {
-#     "bookName": "ibnmajah",
-#     "hadithNumber": "1",
-#     "hadithText": "The Book of the Sunnah",
-#     "hadithReference": "English reference : Vol. 1, Book 1, Hadith 1",
-#     "hadithNarrated": "Narrated"
-# }
-# Save the json objects in a file named ibnmajah1.json
+from fetch_collections import fetch_page
 
 
 def read_file(file_name: str) -> str:
@@ -34,6 +19,9 @@ def parse_file(file_name: str) -> BeautifulSoup:
 
 
 def get_book_name(soup: BeautifulSoup) -> str:
+    """
+    Returns the book name from the soup object
+    """
     div_tag_book_name = soup.find('div', class_="actualHadithContainer")
     a_tag = div_tag_book_name.find('a')
 
@@ -46,6 +34,9 @@ def get_book_name(soup: BeautifulSoup) -> str:
 
 
 def get_hadith_number(soup: BeautifulSoup) -> str:
+    """
+    Returns the hadith number from the soup object
+    """
     div_tag_book_name = soup.find('div', class_="actualHadithContainer")
     a_tag = div_tag_book_name.find('a')
 
@@ -58,6 +49,9 @@ def get_hadith_number(soup: BeautifulSoup) -> str:
 
 
 def get_hadith_text(soup: BeautifulSoup) -> str:
+    """
+    Returns the hadith text from the soup object
+    """
     text_details_div = soup.find('div', class_='text_details')
     if text_details_div:
         return text_details_div.text.strip()
@@ -65,6 +59,9 @@ def get_hadith_text(soup: BeautifulSoup) -> str:
 
 
 def get_hadith_reference(soup: BeautifulSoup) -> str:
+    """
+    Returns the hadith reference from the soup object
+    """
     hadith_reference_div = soup.find('div', class_='hadith_reference_sticky')
     if hadith_reference_div:
         return hadith_reference_div.text.strip()
@@ -72,6 +69,9 @@ def get_hadith_reference(soup: BeautifulSoup) -> str:
 
 
 def get_hadith_narrated(soup: BeautifulSoup) -> str:
+    """
+    Returns the hadith narrated from the soup object
+    """
     hadith_narrated_div = soup.find('div', class_='hadith_narrated')
     if hadith_narrated_div:
         return hadith_narrated_div.text.strip()
@@ -79,6 +79,9 @@ def get_hadith_narrated(soup: BeautifulSoup) -> str:
 
 
 def get_hadith(soup: BeautifulSoup) -> dict:
+    """
+    Returns the hadith from the soup object
+    """
     return {
         "bookName": get_book_name(soup),
         "hadithNumber": get_hadith_number(soup),
@@ -88,49 +91,38 @@ def get_hadith(soup: BeautifulSoup) -> dict:
     }
 
 
+def check_pages(soup: BeautifulSoup) -> list:
+    """
+    Checks if pagination exists and returns the lists of pages to fetch
+    """
+    pagination_div = soup.find('ul', class_='yiiPager')
+    links = []
+    if pagination_div:
+        page_links = pagination_div.find_all('a')
+        for page_link in page_links:
+            links.append("https://sunnah.com"+page_link['href'])
+        links.pop(0)
+        links.pop(-1)
+    return links
+
+
 def get_hadiths(soup: BeautifulSoup) -> list:
+    """
+    Returns the hadiths from the soup object
+    """
     output_list = []
     boh_divs = soup.find_all('div', class_='boh')
     for boh_div in boh_divs:
         output_list.append(get_hadith(boh_div))
-    return output_list
+    return json.dumps(output_list, indent=4)
 
 
-results = soup.find_all('div', class_='actualHadithContainer')
-output_list = []
-boh_divs = soup.find_all('div', class_='boh')
-for boh_div in boh_divs:
-    output = {}
-    # Extract bookName and hadithNumber
-    div_tag_book_name = boh_div.find('div', class_="actualHadithContainer")
-    a_tag = div_tag_book_name.find('a')
+if __name__ == "__main__":
+    homepage = parse_file('./pages/search.html')
+    pages = check_pages(homepage)
+    print(pages)
+    if pages:
+        for page in range(0, len(pages)):
+            fetch_page(pages[page], f"page{page+1}.html")
 
-    if a_tag:
-        pattern = r'/(?P<book>\w+)(?P<number>[:/\d]+)'
-        match = re.search(pattern, a_tag['href'])
-        if match:
-            output['bookName'] = match[1]
-            output['hadithNumber'] = match[2]
-        else:
-            output['bookName'] = "Unknown"
-            output['hadithNumber'] = "Unknown"
-
-    # Extract hadithText
-    text_details_div = boh_div.find('div', class_='text_details')
-    if text_details_div:
-        output['hadithText'] = text_details_div.text.strip()
-
-    # Extract hadithReference
-    hadith_reference_div = boh_div.find(
-        'div', class_='hadith_reference_sticky')
-    if hadith_reference_div:
-        output['hadithReference'] = hadith_reference_div.text.strip()
-
-    # Extract hadithNarrated
-    hadith_narrated_div = boh_div.find('div', class_='hadith_narrated')
-    if hadith_narrated_div:
-        output['hadithNarrated'] = hadith_narrated_div.text.strip()
-
-    output_list.append(output)
-
-print(json.dumps(output_list, indent=4, ensure_ascii=False))
+    print(get_hadiths(homepage))
